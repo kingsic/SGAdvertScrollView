6eb5fb7db7a0d3375693e05f27af2e02750f6905
kingsic <kingsic@kingsicdeMacBook-Pro.local>
2017-08-13 10:10:57 +0800

    v 1.2.3

--- SGAdvertScrollView/SGAdvertScrollView.h
+++ SGAdvertScrollView/SGAdvertScrollView.h
@@ -5,7 +5,7 @@
 //      GitHub：https://github.com/kingsic/SGAdvertScrollView.git
 //
 //  SGAdvertScrollView.h
-//  Version 1.2.2
+//  Version 1.2.3
 //
 //  Created by kingsic on 17/3/8.
 //  Copyright © 2017年 kingsic. All rights reserved.
@@ -15,7 +15,9 @@
 @class SGAdvertScrollView;
 
 typedef enum : NSUInteger {
+    /// 一行文字滚动样式
     SGAdvertScrollViewStyleNormal,
+    /// 二行文字滚动样式
     SGAdvertScrollViewStyleMore,
 } SGAdvertScrollViewStyle;
 
@@ -26,35 +28,35 @@ typedef enum : NSUInteger {
 @end
 
 @interface SGAdvertScrollView : UIView
-/** delegate */
+/** SGAdvertScrollViewDelegate */
 @property (nonatomic, weak) id<SGAdvertScrollViewDelegate> delegate;
 /** 默认 SGAdvertScrollViewStyleNormal 样式 */
 @property (nonatomic, assign) SGAdvertScrollViewStyle advertScrollViewStyle;
-/** 滚动时间间隔(默认 3s) */
+/** 滚动时间间隔，默认为3s */
 @property (nonatomic, assign) CFTimeInterval scrollTimeInterval;
-/** SGAdvertScrollViewStyleNormal 样式，才有效 */
+/** SGAdvertScrollViewStyleNormal 样式下的左边标志图片数组 */
 @property (nonatomic, strong) NSArray *signImages;
-/** SGAdvertScrollViewStyleNormal 样式，才有效 */
+/** SGAdvertScrollViewStyleNormal 样式下的标题数组 */
 @property (nonatomic, strong) NSArray *titles;
-/** 标题字体颜色(默认 黑色), SGAdvertScrollViewStyleNormal 样式，才有效 */
+/** SGAdvertScrollViewStyleNormal 样式下的标题字体颜色，默认为黑色 */
 @property (nonatomic, strong) UIColor *titleColor;
-/** 标题文字是否居中 (默认为 No)，SGAdvertScrollViewStyleNormal 样式，才有效且不对 signImages 起作用 */
+/** SGAdvertScrollViewStyleNormal 样式下的标题文字是否居中，默认为 No，仅仅针对 titles 起作用 */
 @property (nonatomic, assign) BOOL isTextAlignmentCenter;
 
-/** SGAdvertScrollViewStyleMore 样式，才有效 */
+/** SGAdvertScrollViewStyleMore 样式下的顶部左边标志图片数组 */
 @property (nonatomic, strong) NSArray *topSignImages;
-/** SGAdvertScrollViewStyleMore 样式，才有效 */
+/** SGAdvertScrollViewStyleMore 样式下的顶部标题数组 */
 @property (nonatomic, strong) NSArray *topTitles;
-/** SGAdvertScrollViewStyleMore 样式，才有效 */
+/** SGAdvertScrollViewStyleMore 样式下的底部左边标志图片数组 */
 @property (nonatomic, strong) NSArray *bottomSignImages;
-/** SGAdvertScrollViewStyleMore 样式，才有效 */
+/** SGAdvertScrollViewStyleMore 样式下的底部标题数组 */
 @property (nonatomic, strong) NSArray *bottomTitles;
-/** 标题字体颜色(默认 黑色), SGAdvertScrollViewStyleMore 样式，才有效 */
+/** SGAdvertScrollViewStyleMore 样式下的顶部标题字体颜色，默认为黑色 */
 @property (nonatomic, strong) UIColor *topTitleColor;
-/** 标题字体颜色(默认 黑色), SGAdvertScrollViewStyleMore 样式，才有效 */
+/** SGAdvertScrollViewStyleMore 样式下的底部标题字体颜色，默认为黑色 */
 @property (nonatomic, strong) UIColor *bottomTitleColor;
 
-/** 标题字体大小(默认 13) */
+/** 标题字体大小，默认为13号字体 */
 @property (nonatomic, strong) UIFont *titleFont;
 
 @end
